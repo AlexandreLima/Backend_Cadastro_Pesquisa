4427752b9a85b7801bb498dbdd7dc3acc2821aee
AlexandreLima <xandebre@gmail.com>
2016-08-18 00:59:56 -0300

    Adicionando as classes de teste

---
+++ Backend_Cadastro_Pesquisa.Tests/Repositorios/Pesquisa/PesquisaRepositoriosTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Backend_Cadastro_Pesquisa.Tests.Repositorios.Pesquisa
+{
+    [TestClass]
+    public class PesquisaRepositoriosTest
+    {
+        [TestMethod]
+        public void TestandoCadastro()
+        {
+        }
+    }
+}
