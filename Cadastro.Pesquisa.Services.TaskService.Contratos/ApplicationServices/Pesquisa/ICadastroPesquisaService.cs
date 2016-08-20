9d24149d82ddc2833cb511e37015ad9ff8c1da83
AlexandreLima <xandebre@gmail.com>
2016-08-20 00:43:40 -0300

    Incluindo camadas de serviço(Padrão DDD)

---
+++ Cadastro.Pesquisa.Services.TaskService.Contratos/ApplicationServices/Pesquisa/ICadastroPesquisaService.cs
@@ -0,0 +1,15 @@
+﻿using Cadastro.Pesquisa.Services.Contratos.Pesquisa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro.Pesquisa.Services.TaskService.Contratos.ApplicationServices.Pesquisa
+{
+    public interface ICadastroPesquisaService
+    {
+        bool IncluiPesquisa(PesquisaDto dto);
+        bool obtemPesquisa(PesquisaDto dto);
+    }
+}
