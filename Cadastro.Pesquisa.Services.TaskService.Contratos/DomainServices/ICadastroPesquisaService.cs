3134ef0e9ddefa9ad2b81bfc2a7e2372e2a3e4e8
AlexandreLima <xandebre@gmail.com>
2016-08-20 09:05:29 -0300

    Incluindo adapter, serviço de dominio etc

---
+++ Cadastro.Pesquisa.Services.TaskService.Contratos/DomainServices/ICadastroPesquisaService.cs
@@ -0,0 +1,17 @@
+﻿using Cadastro.Pesquisa.Services.Contratos.Pesquisa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro.Pesquisa.Services.TaskService.Contratos.DomainServices.Pesquisa
+{
+    public interface ICadastroPesquisaService
+    {
+        Services.Contratos.Pesquisa.PesquisaDto IncluiPesquisa(PesquisaDto dto);
+        Services.Contratos.Pesquisa.PesquisaDto ObtemPesquisa(PesquisaDto dto);
+        Services.Contratos.Pesquisa.PesquisaDto ObtemPorId(int id);
+
+    }
+}
